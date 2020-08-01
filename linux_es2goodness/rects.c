17a4734e54846994d3ee84393b2054169f5484d5
masterzorag <masterzorag@gmail.com>
2020-08-01 15:53:37 +0200

    RenderFillRects() as SDL2 would be on GLES2
    - sample to show SDL_FillRects implementation in baremetal GLES2_renderer

---
+++ linux_es2goodness/rects.c
@@ -0,0 +1,164 @@
+/*
+    mimic an SDL_Rect
+
+    standard GLES2 code to draw FillRects
+*/
+
+#if defined (__PS4__)
+
+#include <ps4sdk.h>
+#include <debugnet.h>
+#define  fprintf  debugNetPrintf
+#define  ERROR    DEBUGNET_ERROR
+#define  DEBUG    DEBUGNET_DEBUG
+#define  INFO     DEBUGNET_INFO
+
+
+#elif defined HAVE_LIBAO // on pc
+
+#include <stdio.h>
+#define  debugNetPrintf  fprintf
+#define  ERROR           stderr
+#define  DEBUG           stdout
+#define  INFO            stdout
+
+#include "defines.h"
+
+#endif
+
+// Clang Extended Vectors
+typedef float vec2 __attribute__((ext_vector_type(2)));
+typedef float vec4 __attribute__((ext_vector_type(4)));
+
+/* simple shaders */
+static const char *vs =
+    "precision mediump float;"
+    "attribute vec4 a_Position;"
+    "uniform   vec4 u_color;"
+    //use your own output instead of gl_FrontColor
+    "varying   vec4 fragColor;"
+    ""
+    "void main(void)"
+    "{"
+    "  fragColor   = u_color;"
+    "  gl_Position = a_Position;"
+    "}";
+
+static const char *fs =
+/// 1. default, use texture color
+    "precision mediump float;"
+    "varying   vec4    fragColor;"
+    ""
+    "void main(void)"
+    "{"
+    "  gl_FragColor = fragColor;"
+    "}";
+
+static GLuint simpleProgram = 0;
+static vec2   resolution;
+static vec4   color = { 1., 0., .5, 1. }; // red RGBA
+// shaders locations
+static GLint  a_position_location;
+static GLint  u_color_location;
+
+typedef struct
+{
+    float h;
+    float w;
+    float x;
+    float y;
+} SDL_FRect;
+
+static int
+ORBIS_RenderFillRects(
+    // SDL_Renderer *renderer,
+    const SDL_FRect *rects, int count)
+{
+    GLfloat vertices[8];
+    int idx;
+
+    glUseProgram(simpleProgram);
+
+    //glDisable(GL_CULL_FACE);
+    // enable alpha
+    glEnable(GL_BLEND);
+    glBlendEquation(GL_FUNC_ADD);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glActiveTexture(0);
+
+    /* emit a triangle strip for each rectangle */
+    for (idx = 0; idx < count; ++idx) {
+        const SDL_FRect *rect = &rects[idx];
+
+        GLfloat xMin = rect->x,  xMax = (rect->x + rect->w),
+                yMin = rect->y,  yMax = (rect->y + rect->h);
+        /* (x, y) for 4 points: 8*/
+        vertices[0] = xMin;  vertices[1] = yMin;
+        vertices[2] = xMax;  vertices[3] = yMin;
+        vertices[4] = xMin;  vertices[5] = yMax;
+        vertices[6] = xMax;  vertices[7] = yMax;
+        /* each (vec2)point comes from pairs of floats */
+        glVertexAttribPointer    (a_position_location, 2, GL_FLOAT, GL_FALSE, 0, vertices);
+        glEnableVertexAttribArray(a_position_location);
+        /* write color to use to the shader location */
+        glUniform4f(u_color_location, color.r, color.g, color.b, color.a);
+        /* floats pairs for points from 0-4 */
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    }
+
+    glDisable(GL_BLEND);
+    glUseProgram(0);
+
+    return 0;
+}
+
+void ORBIS_RenderFillRects_init( int width, int height )
+{
+    resolution = (vec2){ width, height };
+
+    simpleProgram = BuildProgram(vs, fs);
+    // select for setup
+    glUseProgram(simpleProgram);
+    // gles2 attach shader locations
+    a_position_location = glGetAttribLocation (simpleProgram, "a_Position");
+    u_color_location    = glGetUniformLocation(simpleProgram, "u_color");
+    // reshape
+    glViewport(0, 0, width, height);
+}
+
+
+static vec4 px_pos_to_normalized(vec2 *pos, vec2 *size)
+{
+    vec4 n; // 2 points .xy pair: (x, y),  (x + texture.w, y + texture.h)
+
+    n.xy  = -1. + 2. / resolution * (*pos); // (-1,-1) is BOTTOMLEFT, (1,1) is UPRIGHT
+    n.zw  = *size / resolution;
+    n.yw *= -1.; // flip Y axis
+//  printf("%f,%f,%f,%f\n", n.x, n.y, n.w, n.w);
+    return n;
+}
+
+#define COUNT  6
+void ORBIS_RenderFillRects_rndr(void)
+{
+    SDL_FRect r[COUNT];
+    vec4 nr;
+    // fill in some pos, size
+    for (int i = 0; i < COUNT; ++i)
+    {
+        vec2 p = (vec2) { 100. + i * 20., 100. + i * 20. },  // position in px
+             s = (vec2) {  10. + i * 10,   10. + i * 20. };  // size in px
+        /* convert to normalized coordinates */
+            nr = px_pos_to_normalized( &p, &s );
+        r[i].x = nr.x, r[i].y = nr.y;
+        r[i].w = nr.z, r[i].h = nr.w;
+    }
+    // gles render all rects
+    ORBIS_RenderFillRects(r, COUNT);
+}
+
+void ORBIS_RenderFillRects_fini(void)
+{
+    if (simpleProgram) glDeleteProgram(simpleProgram), simpleProgram = 0;
+}
